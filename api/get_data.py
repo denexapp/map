cc22ac29c56c33fb6e64df9a6ee3f682fed6c149
Pana_Irina <pana@skbkontur.ru>
2022-10-27 20:38:11 +0500

    variables moved from init

--- api/get_data.py
+++ api/get_data.py
@@ -5,20 +5,19 @@ from http.server import BaseHTTPRequestHandler
 
 
 class handler(BaseHTTPRequestHandler):
-    def __init__(self):
-        self.GoogleSheetsApiKey = os.environ['GOOGLE_SHEETS_API_KEY']
-        self.ranges = ['A:C']
-
     def do_POST(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        
+
+        GoogleSheetsApiKey = os.environ['GOOGLE_SHEETS_API_KEY']
+        ranges = ['A:C']
+
         spreadsheet_id = str(self.rfile.read(int(self.headers.get('Content-Length'))))
-        service = build('sheets', 'v4', developerKey=self.GoogleSheetsApiKey)
+        service = build('sheets', 'v4', developerKey=GoogleSheetsApiKey)
         request = service.spreadsheets().values().batchGet(
             spreadsheetId=spreadsheet_id,
-            ranges=self.ranges
+            ranges=ranges
         )
         response = request.execute()
         response_values = response.get('valueRanges', list())[0].get('values', list())
